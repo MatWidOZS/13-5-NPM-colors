2f515d9e41823e017a2ae36a115770319bbf413b
MatWidOZS <36623352+MatWidOZS@users.noreply.github.com>
2018-04-06 21:55:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var OSinfo = require('./modules/OSinfo');
 
-process.stdin.setEncoding('utf-8'); //ustawienie enkodowania na UTF-8 (domyślnie szesnastkowe)
+process.stdin.setEncoding('utf-8');
 
 console.log('\n\nType one of these commands: \n"/version" - see your Node version, \n"/language" - see your system language, \n"/getOSinfo" - get information about your computer, \n"/exit" - quit app.');
 
 process.stdin.on('readable', function() {
-	var input = process.stdin.read(); //read - odczytanie, co użytkownik podał na wejściu
+	var input = process.stdin.read();
 	if (input !== null) {
 		var instruction = input.toString().trim();
 		switch (instruction) {
@@ -20,7 +20,6 @@ process.stdin.on('readable', function() {
 				process.exit();
 				break;
 			case '/getOSinfo':
-				// process.stdout.write('Tutaj będzie info o systemie!\n');
 				OSinfo.print();
 				break;
 			default:
